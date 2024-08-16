19ce8246df47b03a731b49819388216c8250db51
gbejarl <74567115+gbejarl@users.noreply.github.com>
2024-08-16 10:15:06 -0400

    Fixed plots

--- laharml_plots.py
+++ laharml_plots.py
@@ -54,8 +54,11 @@ def plot_interval(trace,
                  y=trace.data,
                  ax=ax,
                  color='black')
-    if starttime and endtime:
+    if starttime or endtime:
         ax.axvspan(starttime, endtime, color='red', alpha=0.15)
+    else:
+        starttime = pd.to_datetime(trace.times('timestamp')[0], unit='s')
+        endtime = pd.to_datetime(trace.times('timestamp')[-1], unit='s')
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
     formatter = mdates.DateFormatter('%H:%M')
     ax.xaxis.set_major_formatter(formatter)
